c80ea0e170f0c88fe6e261db9db0e17cbd1eb3fa
Naveeng7 <68994263+Naveeng7@users.noreply.github.com>
2020-08-11 12:04:23 +0530

    Update foobar3.1.py

--- foobar3.1.py
+++ foobar3.1.py
@@ -1,45 +1,18 @@
-import time
-import math
-
 def solution(n):
-    solution.conter = 0
+    solution.sm = n
     lst = [0] * n
+    solution.conter = 0
 
-    def proces(lst, nextindex, n, redsize, conter):
-        if redsize < 0:
-            return
-        nlst = list()
-        if redsize == 0 :
-            for i in range(nextindex):
-                nlst.append(lst[i])
-            dist = 1
-            for i in range(len(nlst)):
-                for j in range(len(nlst)):
-                    if i != j:
-                        if nlst[i] == nlst[j]:
-                            dist = 0
-            if len(nlst) >= 2 and dist == 1:
-                if sum(nlst) == n:
-                    solution.conter = solution.conter + 1
-            nlst.clear()
-            return
+    def recur(i, n, lst, index):
+        if n == 0:
+            if len(lst[:index]) >= 2 and sum(lst[:index]) == solution.sm:
+                solution.conter = solution.conter + 1
+        for j in range(i, n + 1):
+            lst[index] = j
+            if lst[index] == lst[index - 1]:
+                continue
+            else:
+                recur(j, n - j, lst, index + 1)
 
-        if (nextindex == 0):
-            pre =1
-        else :
-            pre = lst[nextindex-1]
-
-        for k in range(pre, n+1):
-            lst[nextindex] = k
-            print('running')
-            proces(lst, nextindex+1, n, redsize-k, solution.conter)
-
-
-    proces(lst, 0, n, n, solution.conter)
+    recur(1, n, lst, 0)
     return solution.conter
-
-n = 200
-s = time.time()
-print(solution(n))
-e = time.time()
-print('timetaken',math.floor(e-s),'seconds')
